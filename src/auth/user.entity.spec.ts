b988a4fbe3d42a81204623625d5d66c10e72eba7
alex.cauble <alexander.t.cauble@gmail.com>
2020-03-22 15:34:08 -0500

    added user properties tests

--- src/auth/user.entity.spec.ts
+++ src/auth/user.entity.spec.ts
@@ -6,6 +6,7 @@ describe('user entity', () => {
 
   beforeEach(() => {
     user = new User();
+    user.username = 'testUsername';
     user.password = 'testPassword';
     user.salt = 'testSalt';
     bcrypt.hash = jest.fn();
@@ -30,6 +31,36 @@ describe('user entity', () => {
 
       expect(bcrypt.hash).toHaveBeenLastCalledWith('123456', 'testSalt');
       expect(result).toEqual(false);
+    });
+  });
+
+  describe('User properties', () => {
+    beforeEach(() => {
+      user.id = 1;
+      user.salt = 'testSalt';
+      user.tasks = [];
+    });
+    afterEach(() => {
+      user = undefined;
+    });
+    it('should be an instance of class User', () => {
+      expect(user).toBeInstanceOf(User);
+    });
+    it('should have id of type number', () => {
+      user.id = 1;
+      expect(user).toHaveProperty('id', user.id);
+    });
+    it('should have a username of type string', () => {
+      expect(user).toHaveProperty('username', 'testUsername');
+    });
+    it('should have a password of type string', () => {
+      expect(user).toHaveProperty('password', 'testPassword');
+    });
+    it('should have a salt of type string', () => {
+      expect(user).toHaveProperty('salt', 'testSalt');
+    });
+    it('should have property tasks of type array', () => {
+      expect(user).toHaveProperty('tasks', []);
     });
   });
 });
